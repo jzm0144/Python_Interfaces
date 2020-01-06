2eca7fb2c6e5544c548099f0a52769c81d8e8185
Janzaib Masood <jzm0144@auburn.edu>
2020-01-05 20:26:50 -0600

    GUI with Grid

---
+++ grid.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel1 = Label(root, text="Hellow World!")
+myLabel2 = Label(root, text="My name is Janzaib")
+
+# Shoving it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+
+
+
+root.mainloop()
