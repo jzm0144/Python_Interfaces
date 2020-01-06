c7691af01662ed3c9d712bb259e6e1cdc5f5edf8
Janzaib Masood <jzm0144@auburn.edu>
2020-01-05 22:27:46 -0600

    Addition Calculator

--- calc.py
+++ calc.py
@@ -17,20 +17,45 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
+    global math
+    math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
 
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
-    e.insert(0, str(f_num + int(second_number)))
+    if math == "addition":
+        e.insert(0, str(f_num + int(second_number)))
+    if math == "subtraction":
+        e.insert(0, str(f_num - int(second_number)))
+    if math == "multiplication":
+        e.insert(0, str(f_num * int(second_number)))
+    if math == "division":
+        e.insert(0, str(f_num / int(second_number)))
 
 def button_subtract():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
 def button_multiply():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
 def button_divide():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 # Define Buttons
 button_1 = Button(root, text="1", padx=40, pady = 20, command=lambda: button_click(1), fg='blue')
@@ -43,14 +68,14 @@ button_7 = Button(root, text="7", padx=40, pady = 20, command=lambda: button_cli
 button_8 = Button(root, text="8", padx=40, pady = 20, command=lambda: button_click(8), fg='blue')
 button_9 = Button(root, text="9", padx=40, pady = 20, command=lambda: button_click(9), fg='blue')
 button_0 = Button(root, text="0", padx=40, pady = 20, command=lambda: button_click(0), fg='blue')
-button_equal = Button(root, text="=", padx=91, pady = 20, command=button_equal, fg='blue')
-button_clear = Button(root, text="Clear", padx=79, pady = 20, command=button_clear, fg='blue')
+button_equal = Button(root, text="=", padx=98, pady = 20, command=button_equal, fg='blue')
+button_clear = Button(root, text="Clear", padx=86, pady = 20, command=button_clear, fg='blue')
 
 
 button_add = Button(root, text="+", padx=39, pady = 20, command=button_add, fg='blue')
-button_subtract = Button(root, text="+", padx=39, pady = 20, command=button_subtract, fg='blue')
-button_multiply = Button(root, text="+", padx=39, pady = 20, command=button_multiply, fg='blue')
-button_divide = Button(root, text="+", padx=39, pady = 20, command=button_divide, fg='blue')
+button_subtract = Button(root, text="-", padx=41, pady = 20, command=button_subtract, fg='blue')
+button_multiply = Button(root, text="x", padx=40, pady = 20, command=button_multiply, fg='blue')
+button_divide = Button(root, text="/", padx=40, pady = 20, command=button_divide, fg='blue')
 
 
 
