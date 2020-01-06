7de8aba3e321f0d522909a5a0ae3c44f6e1e2162
Janzaib Masood <jzm0144@auburn.edu>
2020-01-05 22:16:52 -0600

    Addition Calculator

--- calc.py
+++ calc.py
@@ -24,9 +24,13 @@ def button_equal():
     second_number = e.get()
     e.delete(0, END)
     e.insert(0, str(f_num + int(second_number)))
-    
 
-
+def button_subtract():
+    return
+def button_multiply():
+    return
+def button_divide():
+    return
 
 # Define Buttons
 button_1 = Button(root, text="1", padx=40, pady = 20, command=lambda: button_click(1), fg='blue')
@@ -39,9 +43,17 @@ button_7 = Button(root, text="7", padx=40, pady = 20, command=lambda: button_cli
 button_8 = Button(root, text="8", padx=40, pady = 20, command=lambda: button_click(8), fg='blue')
 button_9 = Button(root, text="9", padx=40, pady = 20, command=lambda: button_click(9), fg='blue')
 button_0 = Button(root, text="0", padx=40, pady = 20, command=lambda: button_click(0), fg='blue')
-button_add = Button(root, text="+", padx=39, pady = 20, command=button_add, fg='blue')
 button_equal = Button(root, text="=", padx=91, pady = 20, command=button_equal, fg='blue')
 button_clear = Button(root, text="Clear", padx=79, pady = 20, command=button_clear, fg='blue')
+
+
+button_add = Button(root, text="+", padx=39, pady = 20, command=button_add, fg='blue')
+button_subtract = Button(root, text="+", padx=39, pady = 20, command=button_subtract, fg='blue')
+button_multiply = Button(root, text="+", padx=39, pady = 20, command=button_multiply, fg='blue')
+button_divide = Button(root, text="+", padx=39, pady = 20, command=button_divide, fg='blue')
+
+
+
 # Put the Buttons on the Screen
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
@@ -57,9 +69,11 @@ button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
-
+button_add.grid(row=5, column=0)
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 
 root.mainloop()
