d3afdbbf6521af4083fdf114be208f7e087b2901
Janzaib Masood <jzm0144@auburn.edu>
2020-01-05 20:45:22 -0600

    GUI with Buttons

--- grid.py
+++ grid.py
@@ -8,6 +8,6 @@ myLabel2 = Label(root, text="My name is Janzaib")
 
 # Shoving it onto the screen
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=1)
+myLabel2.grid(row=1, column=5)
 
 root.mainloop()
